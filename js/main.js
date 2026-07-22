22398aa3ac431af7b399d947ebc4ebb4244eccea
NightCastir <tomasgermany2580@gmail.com>
2026-07-22 16:43:56 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,62 @@
 // NightCast v2
 
-document.addEventListener(
-"DOMContentLoaded",
-function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-console.log("NightCast v2 Ready");
+
+fetch("../data/social.json")
+
+.then(response => response.json())
+
+.then(data => {
+
+
+const socialBox = document.querySelector(".social-grid");
+
+
+if(!socialBox) return;
+
+
+socialBox.innerHTML = "";
+
+
+data.forEach(item => {
+
+
+socialBox.innerHTML += `
+
+<a href="${item.url}" target="_blank" class="social-card">
+
+
+<div class="platform">
+
+<i class="fa-solid fa-radio"></i>
+
+${item.name}
+
+</div>
+
+
+<p>
+ورود به صفحه رسمی NightCast در ${item.name}
+</p>
+
+
+</a>
+
+`;
+
+
+});
+
+
+})
+
+
+.catch(error => {
+
+console.log("Social Load Error:", error);
+
+});
+
 
 });

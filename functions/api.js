ac2f8adb210ff4a80ab72b8707321e6138a0a3e9
NightCastir <tomasgermany2580@gmail.com>
2026-07-24 23:32:30 +0330

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,1 +1,12 @@
-
+export async function onRequest(context) {
+  return new Response(
+    JSON.stringify({
+      status: "NightCast API Ready"
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}

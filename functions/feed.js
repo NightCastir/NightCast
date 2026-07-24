c3acc4dcb286ae401afc459bf038c3359e7120fd
NightCastir <tomasgermany2580@gmail.com>
2026-07-24 23:16:40 +0330

    Update feed.js

--- functions/feed.js
+++ functions/feed.js
@@ -1,27 +1,23 @@
 export async function onRequestGet() {
 
-  const posts = [
-
+  return Response.json([
     {
       id: 1,
       title: "SPIN Selling",
+      description: "خلاصه کتاب فروش به روش اسپین",
+      image: "/assets/images/spin.webp",
       platform: "YouTube",
-      image: "https://nightcast.ir/assets/images/spin.webp",
-      url: "https://youtube.com/@NightCast-r5e",
-      date: "2026-07-24"
+      date: "2026-07-24",
+      url: "https://youtube.com/@NightCast-r5e"
     },
-
     {
       id: 2,
       title: "قدرت عادت",
+      description: "خلاصه کتاب قدرت عادت",
+      image: "/assets/images/habit.webp",
       platform: "Telegram",
-      image: "https://nightcast.ir/assets/images/habit.webp",
-      url: "https://t.me/NightCast_ir",
-      date: "2026-07-23"
+      date: "2026-07-23",
+      url: "https://t.me/NightCast_ir"
     }
-
-  ];
-
-  return Response.json(posts);
-
+  ]);
 }

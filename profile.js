0be8bdc8b16f281cddbd47d202b7d34cf6fa1bd6
NightCastir <tomasgermany2580@gmail.com>
2026-07-29 15:50:38 +0330

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,28 @@
+const user = JSON.parse(localStorage.getItem("user"));
 
+if(user){
+
+document.getElementById("userName").textContent =
+user.name || "NightCast User";
+
+document.getElementById("userEmail").textContent =
+user.email || user.phone;
+
+document.getElementById("avatar").textContent =
+(user.name || "N")
+.charAt(0)
+.toUpperCase();
+
+}
+
+document
+.getElementById("logout")
+.onclick=function(){
+
+localStorage.removeItem("token");
+
+localStorage.removeItem("user");
+
+location.href="index.html";
+
+};

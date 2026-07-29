e7b6fe8a53c447962b5b17238c96af7514af7cba
NightCastir <tomasgermany2580@gmail.com>
2026-07-29 15:59:28 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,67 @@
+const API =
+"https://nightcast-api.tomasgermany2580.workers.dev";
 
+const token =
+localStorage.getItem("token");
+
+if (!token) {
+
+    location.replace("login.html");
+
+} else {
+
+    checkLogin();
+
+}
+
+async function checkLogin() {
+
+    try {
+
+        const res = await fetch(
+
+            API + "/api/me",
+
+            {
+
+                headers: {
+
+                    Authorization: "Bearer " + token
+
+                }
+
+            }
+
+        );
+
+        const data = await res.json();
+
+        if (!data.success) {
+
+            localStorage.clear();
+
+            location.replace("login.html");
+
+            return;
+
+        }
+
+        localStorage.setItem(
+
+            "user",
+
+            JSON.stringify(data.user)
+
+        );
+
+    }
+
+    catch (e) {
+
+        console.log(e);
+
+        location.replace("login.html");
+
+    }
+
+}

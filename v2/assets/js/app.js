23efe1e9626b2ca5cb624a63d4d500898def8b9f
NightCastir <tomasgermany2580@gmail.com>
2026-07-31 11:08:11 +0330

    Update app.js

--- v2/assets/js/app.js
+++ v2/assets/js/app.js
@@ -1,1 +1,32 @@
+/*
+──────────────────────────────────────────────
+ NightCast V2
+ Main Application
+──────────────────────────────────────────────
+*/
 
+'use strict';
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    await App.init();
+
+});
+
+const App = {
+
+    async init() {
+
+        Header.init();
+
+        Feed.init();
+
+        Footer.init();
+
+        Player.init();
+
+        Router.init();
+
+    }
+
+};

125828c7449f97f38445741f786e5149d078e3fa
NightCastir <tomasgermany2580@gmail.com>
2026-07-31 11:10:02 +0330

    Update header.js

--- v2/assets/js/components/header.js
+++ v2/assets/js/components/header.js
@@ -1,1 +1,31 @@
+const Header = {
 
+    init() {
+
+        this.render();
+
+        this.events();
+
+    },
+
+    render() {
+
+        document
+            .getElementById("header")
+            .innerHTML = `
+
+<div class="header-container">
+
+    HEADER
+
+</div>
+
+`;
+
+    },
+
+    events() {
+
+    }
+
+};

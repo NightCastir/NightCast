71b74183a9883e536e70ae39c9d06d58b60db100
NightCastir <tomasgermany2580@gmail.com>
2026-07-31 11:17:52 +0330

    Update api.js

--- v2/assets/js/services/api.js
+++ v2/assets/js/services/api.js
@@ -1,1 +1,36 @@
+/*
+────────────────────────────────────────
+NightCast API Service
+────────────────────────────────────────
+*/
 
+'use strict';
+
+const API = {
+
+    // آدرس Worker را فقط اینجا تغییر می‌دهی
+    BASE_URL: "https://nightcast-api.tomasgermany2580.workers.dev",
+
+    async getEpisodes(page = 1, limit = 12) {
+
+        const response = await fetch(
+
+            `${this.BASE_URL}/api/episodes?page=${page}&limit=${limit}`,
+
+            {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
+                }
+            }
+
+        );
+
+        if (!response.ok)
+            throw new Error("Network Error");
+
+        return await response.json();
+
+    }
+
+};

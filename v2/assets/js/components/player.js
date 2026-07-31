1de657aebfff8c7722fe2efaae49282cc0726f17
NightCastir <tomasgermany2580@gmail.com>
2026-07-31 15:36:16 +0330

    Update player.js

--- v2/assets/js/components/player.js
+++ v2/assets/js/components/player.js
@@ -1,1 +1,119 @@
+/*
+──────────────────────────────────────────────
+NightCast V2
+Global Audio Player
+──────────────────────────────────────────────
+*/
+
+
+const Player = {
+
+
+    audio: null,
+
+
+    current: null,
+
+
+
+    init(){
+
+
+        this.audio =
+            document.getElementById(
+                "audio-player"
+            );
+
+
+
+        if(!this.audio){
+
+            console.error(
+                "Audio player not found"
+            );
+
+            return;
+
+        }
+
+
+    },
+
+
+
+
+
+
+    play(url){
+
+
+
+        if(!url){
+
+            return;
+
+        }
+
+
+
+        if(this.current !== url){
+
+
+            this.audio.src = url;
+
+
+            this.current = url;
+
+
+        }
+
+
+
+        this.audio.play();
+
+
+
+    },
+
+
+
+
+
+    pause(){
+
+
+        if(this.audio){
+
+            this.audio.pause();
+
+        }
+
+
+    },
+
+
+
+
+
+    stop(){
+
+
+        if(this.audio){
+
+
+            this.audio.pause();
+
+            this.audio.currentTime = 0;
+
+
+        }
+
+
+    }
+
+
+
+};
+
+
 

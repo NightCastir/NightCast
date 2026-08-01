8334c5acc2e76a3eaaa124806bfb482068220d19
NightCastir <tomasgermany2580@gmail.com>
2026-08-01 12:58:15 +0330

    Update feed.js

--- v2/assets/js/components/feed.js
+++ v2/assets/js/components/feed.js
@@ -59,6 +59,8 @@ const Feed = {
                 !result.success
             ) {
 
+                this.hideLoading();
+                
                 this.showError();
 
                 return;

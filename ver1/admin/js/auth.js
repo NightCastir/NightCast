035b293f9c28980ccd6c14f0795c517d3dc9fdbc
NightCastir <tomasgermany2580@gmail.com>
2026-08-02 11:51:39 +0330

    Update auth.js

--- ver1/admin/js/auth.js
+++ ver1/admin/js/auth.js
@@ -1,1 +1,151 @@
+/* =====================
+   NIGHTCAST AUTH
+===================== */
 
+
+
+const NightCastAuth = {
+
+
+
+/*
+ CHECK SESSION
+*/
+
+
+async check(){
+
+
+
+const token =
+
+sessionStorage.getItem(
+"NightCastSession"
+);
+
+
+
+if(!token){
+
+
+window.location.href="login.html";
+
+return false;
+
+
+}
+
+
+
+
+try{
+
+
+const data =
+
+await NightCastAPI.get(
+"/api/admin/me"
+);
+
+
+
+
+
+if(data.logged){
+
+
+
+return data.admin;
+
+
+
+}
+
+
+
+
+
+this.logout();
+
+
+}
+
+
+catch(error){
+
+
+console.error(
+"AUTH ERROR:",
+error
+);
+
+
+
+this.logout();
+
+
+}
+
+
+
+},
+
+
+
+
+
+/*
+ LOGOUT
+*/
+
+
+async logout(){
+
+
+
+try{
+
+
+await NightCastAPI.post(
+
+"/api/admin/logout",
+
+{}
+
+
+);
+
+
+}
+
+catch(error){
+
+
+console.error(error);
+
+
+}
+
+
+
+
+
+sessionStorage.removeItem(
+
+"NightCastSession"
+
+);
+
+
+
+window.location.href=
+
+"login.html";
+
+
+
+}
+
+
+
+};
